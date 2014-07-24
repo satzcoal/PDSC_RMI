2b409c2961cb5fa84cbcc748459415ef1910ddd6
lijingchao <satzcoal@hotmail.com>
2014-07-24 17:52:09 +0800

    1

--- PDSC_RMI_Server/src/main/java/com/cnpat/pdsc/common/Consts.java
+++ PDSC_RMI_Server/src/main/java/com/cnpat/pdsc/common/Consts.java
@@ -11,9 +11,13 @@ public abstract class Consts {
 
 	public static final int BOOK_OPTYPE_ENABLE = 0;
 	public static final int BOOK_OPTYPE_DISABLE = 1;
-	
+
 	public static final int UPDATE_OPTYPE_ADD = 0;
 	public static final int UPDATE_OPTYPE_EDIT = 1;
 	public static final int UPDATE_OPTYPE_ROLLBACK = 2;
-	
+
+	public static final int UPDATE_DATATYPE_CURRENT = 0;
+	public static final int UPDATE_DATATYPE_GRANT = 1;
+	public static final int UPDATE_DATATYPE_PUBLIC = 2;
+
 }
--- PDSC_RMI_Server/src/main/java/com/cnpat/pdsc/service/UpdateDetailService.java
+++ PDSC_RMI_Server/src/main/java/com/cnpat/pdsc/service/UpdateDetailService.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 import org.springframework.orm.hibernate4.HibernateTemplate;
 
-import cn.com.cnpat.cprs.rmi.bean.CurrentBean;
-
+import com.cnpat.pdsc.common.Consts;
 import com.cnpat.pdsc.rmi.msg.UpdateDetailInMsg;
 import com.cnpat.pdsc.rmi.msg.UpdateDetailOutMsg;
 import com.cnpat.pdsc.rmi.msg.entry.CommonEntry;
@@ -16,18 +15,41 @@ public class UpdateDetailService {
 	private HibernateTemplate ht;
 
 	public UpdateDetailOutMsg getUpdateDetail(UpdateDetailInMsg req) {
-		int datatype = req.getDataType();
-		CurrentBean cb = new CurrentBean();
-		switch (datatype) {
 
+		int datatype = req.getDataType();
+		
+		switch (datatype) {
+		case Consts.UPDATE_DATATYPE_CURRENT:
+			makeCurrentBeans(req.getIdList());
+			break;
+		case Consts.UPDATE_DATATYPE_GRANT:
+			makeGrantBeans(req.getIdList());
+			break;
+		case Consts.UPDATE_DATATYPE_PUBLIC:
+			makePublicBeans(req.getIdList());
+			break;
+		default:
+			break;
 		}
 
 		UpdateDetailOutMsg res = new UpdateDetailOutMsg();
 		List<CommonEntry> list = new LinkedList<CommonEntry>();
-		
+
 		res.setResList(list);
 
 		return res;
+	}
+
+	private void makePublicBeans(List<String> idList) {
+
+	}
+
+	private void makeGrantBeans(List<String> idList) {
+
+	}
+
+	private void makeCurrentBeans(List<String> idList) {
+
 	}
 
 	public HibernateTemplate getHt() {
